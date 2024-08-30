42d9e221a28c7b3e2fe05a9560858b5498c3bbdc
Ziyaad Waggie <ziyaadwaggie212@icloud.com>
2024-08-30 17:55:34 +0200

    HTML, CSS and JavaScript changes made.

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        let regex = /^pet_[a-zA-Z0-9]+$/;
+
+    if (regex.test(input)) {
+        result = "Valid Syntax";
+        document.getElementById('status-dot').className = "valid-dot";
+    }
+
+    else {
+        result = "Invalid Syntax";
+        document.getElementById('status-dot').className = "invalid-dot";
+    }
 
             document.getElementById('result').innerText = result;
 }
